5cbbfccaaacd06dd0f98089771934dd597bcec6b
김태건 <nugeat@gimtaegeon-ui-MacBookPro.local>
2021-07-10 18:33:11 +0900

    baekjoon 연습문제

--- 01.python/exercise.py
+++ 01.python/exercise.py
@@ -1,13 +1,116 @@
-score = input()
-score = int(score)
+## if문
+# 2753번
+# year = input()
+# year = int(year)
 
-if 90 <= score <= 100:
-    print("A")
-elif 80 <= score <= 89:
-    print("B")
-elif 70 <= score <= 79:
-    print("C") 
-elif 60 <= score <= 69 :
-    print("D")
-else :
-    print("F")
+# if (year%4 == 0) and (year % 100 != 0 or year % 400 == 0):
+#     print('1')
+# else:
+#     print('0')
+
+# 14681번
+# x = input()
+# y = input()
+# x = int(x)
+# y = int(y)
+
+# if x > 0 and y > 0 :
+#     print(1)
+# elif x > 0 and y < 0:
+#     print(4)
+# elif x < 0 and y > 0:
+#     print(2)
+# else:
+#     print(3)
+
+# 2884번
+# hour, minute = input().split()
+# hour = int(hour)
+# minute = int(minute)
+
+# minute = minute - 45
+# if minute > 0 :
+#     print(hour,minute)
+# elif minute == 0:
+#     print(hour,0)
+# else:
+#     if hour-1 < 0:
+#         print(23,minute +60)
+#     else:
+#         print(hour-1,minute +60)
+
+## for문
+# 2739번
+# N = input()
+# N = int(N)
+
+# for x in range(1,10):
+#     print(N,'*',x,'=',N*x)
+
+### 10950번
+
+# 8393번
+# total = 0
+# nums = input()
+# nums = int(nums)
+# for x in range(1,nums+1):
+#     total += x
+    
+# print(total)
+
+### 15552번
+
+# 2741번
+# N = input()
+# N = int(N)
+# for x in range(1,N+1):
+#     print(x)
+
+# 2742번
+# N = input()
+# N = int(N)
+# a =[]
+# for x in range(1,N+1):
+#     a.append(x)
+    
+# a.sort(reverse=True)
+# for x in a:
+#     print(x)
+
+# 11021번
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
